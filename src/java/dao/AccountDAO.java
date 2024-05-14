c5dd0806758ea931358f836ca079fc0a6e385b39
trangtpdhe172319 <trangtpdhe172319@fpt.edu.vn>
2024-05-14 15:58:50 +0700

    login

---
+++ src/java/dao/AccountDAO.java
@@ -0,0 +1,58 @@
+package dao;
+
+import model.Account;
+
+public class AccountDAO extends DBContext {
+
+    public Account findAccount(Account account) {
+        connection = getConnection();
+        String sql = "SELECT *\n"
+                + "  FROM [dbo].[Account]\n"
+                + "  WHERE [Account].Email = ? AND [Account].Password = ?";
+
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, account.getEmail());
+            statement.setString(2, account.getPassword());
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                account.setAccountID(resultSet.getInt("ID"));
+                account.setUsername(resultSet.getString("Username"));
+                account.setPassword(resultSet.getString("Password"));
+                account.setEmail(resultSet.getString("Email"));
+                account.setRole(resultSet.getInt("RoleID"));
+                account.setPhone(resultSet.getInt("Phone"));
+                account.setImage(resultSet.getString("Image"));
+                account.setName(resultSet.getString("Name"));
+                return account;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        // Tạo một tài khoản mới để tìm kiếm
+        Account accountToFind = new Account();
+        accountToFind.setEmail("trang@gmail.com");
+        accountToFind.setPassword("123");
+
+        // Tạo một đối tượng AccountDAO để thực hiện tìm kiếm
+        AccountDAO accountDAO = new AccountDAO();
+
+        // Gọi phương thức findAccount từ đối tượng DAO để tìm kiếm tài khoản
+        Account foundAccount = accountDAO.findAccount(accountToFind);
+
+        // Kiểm tra xem tài khoản đã tìm thấy hay không
+        if (foundAccount != null) {
+            System.out.println("Account found:");
+            System.out.println("ID: " + foundAccount.getAccountID());
+            System.out.println("Name: " + foundAccount.getName());
+            System.out.println("Email: " + foundAccount.getEmail());
+            System.out.println("Role: " + foundAccount.getRole());
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+}
--- src/java/model/Account.java
+++ src/java/model/Account.java
@@ -1,13 +1,96 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model;
 
-/**
- *
- * @author TienP
- */
+package model;
 public class Account {
+    private int accountID;
+    private String username;
+    private String password;
+    private int role;
+    private int phone;
+    private String email;
+    private String image;
+    private String name;
+
+    public Account() {
+    }
+
+    public Account(int accountID, String username, String password, int role, int phone, String email, String image, String name) {
+        this.accountID = accountID;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.phone = phone;
+        this.email = email;
+        this.image = image;
+        this.name = name;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "accountID=" + accountID + ", username=" + username + ", password=" + password + ", role=" + role + ", phone=" + phone + ", email=" + email + ", image=" + image + ", name=" + name + '}';
+    }
     
 }
