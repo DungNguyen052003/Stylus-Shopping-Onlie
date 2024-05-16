c0c9ba68f88f38e3270c9c5a35d592affe315b90
duongttmhe170982 <duongttmhe170982@fpt.edu.vn>
2024-05-16 11:45:28 +0700

    thêm file register

---
+++ src/java/controller/RegisterServlet.java
@@ -0,0 +1,147 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.AccountDAO;
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Account;
+
+/**
+ *
+ * @author 84976
+ */
+@WebServlet(name = "RegisterServlet", urlPatterns = {"/RegisterServlet"})
+public class RegisterServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RegisterServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet RegisterServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("view/authen/register.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String fName = request.getParameter("name");
+        String uName = request.getParameter("username");
+        String uPass = request.getParameter("pass");
+        String uPho = request.getParameter("phone");
+        String uEmail = request.getParameter("email");
+
+        AccountDAO ac = new AccountDAO();
+        Account account;
+
+        String message = "Something wrong";
+        int slUPrev = ac.getNumberAccounts();
+        boolean isDup = ac.checkUserNameDuplicate(uName);
+        boolean isValidPassword = isValidPassword(uPass);
+
+        if (isValidPassword) {
+            // Xử lý logic lưu thông tin tài khoản vào cơ sở dữ liệu
+            // Ví dụ: bạn có thể sử dụng JDBC để thực hiện việc này
+            // Sau đó chuyển hướng người dùng đến trang đăng nhập hoặc trang chính của ứng dụng
+            if (isDup) {
+                message = "Username already exists!";
+                request.setAttribute("error", message);
+               // request.getRequestDispatcher("view/authen/register.jsp").forward(request, response);
+            } else {
+                // Create an account object with the provided information
+                account = new Account(slUPrev, uName, uPass, 5, Integer.parseInt(uPho), uEmail, null, fName);
+                // Insert the account into the database
+                ac.insert(account);
+                int slUAfter = ac.getNumberAccounts();
+                if (slUAfter > slUPrev) {
+                    message = "Sign Up successful. Please Login!";
+                    request.setAttribute("error", message);
+                }
+                request.getRequestDispatcher("view/authen/login.jsp").forward(request, response);
+            }
+
+        } else {
+            // Nếu mật khẩu không hợp lệ, hiển thị thông báo lỗi
+            message = "8-16 characters at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character";
+            request.setAttribute("errorMessage", message);
+            
+        }
+        request.getRequestDispatcher("view/authen/register.jsp").forward(request, response);
+    }
+
+    // Hàm kiểm tra tính hợp lệ của mật khẩu
+    private boolean isValidPassword(String password) {
+        // Kiểm tra độ dài
+        if (password.length() < 8 || password.length() > 16) {
+            return false;
+        }
+
+        // Kiểm tra chứa ít nhất một chữ cái thường, một chữ cái viết hoa, một chữ số và một ký tự đặc biệt
+        if (!password.matches(".*[a-z].*") && !password.matches(".*[A-Z].*")
+                && !password.matches(".*\\d.*") && !password.matches(".*[!@#$%^&*()-+=`~\\[\\]{}|;:'\",.<>?/\\\\].*")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
