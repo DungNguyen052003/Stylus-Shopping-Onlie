e38a3982034040a2beba3f8574aab20ea1df00f0
trangtpdhe172319 <trangtpdhe172319@fpt.edu.vn>
2024-05-17 16:12:58 +0700

    CustomerDAO

--- src/java/dao/CustomerDAO.java
+++ src/java/dao/CustomerDAO.java
@@ -1,3 +1,8 @@
+package dao;
+import context.DBContext;
+import java.sql.SQLException;
+import model.Customer;
+
 ///*
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 // * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -71,3 +76,54 @@
 //    }
 //
 //}
+public class CustomerDAO extends DBContext {
+
+    public Customer findCustomer(Customer customer) {
+        String sql = "SELECT *\n"
+                + "  FROM [dbo].[Customer]\n"
+                + "  WHERE Email = ? AND Password = ?";
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, customer.getEmail());
+            statement.setString(2, customer.getPassword());
+            resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                customer.setCustomerID(resultSet.getInt("CustomerID"));
+                customer.setUsername(resultSet.getString("Username"));
+                customer.setAddress(resultSet.getString("Address"));
+                customer.setPhone(resultSet.getString("Phone"));
+                customer.setRoleID(resultSet.getInt("RoleID"));
+                customer.setPassword(resultSet.getString("Password"));
+                customer.setName(resultSet.getString("Name"));
+                customer.setImage(resultSet.getString("Image"));
+                return customer;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        // Tạo một tài khoản mới để tìm kiếm
+        Customer accountToFind = new Customer();
+        accountToFind.setEmail("john.doe@example.com");
+        accountToFind.setPassword("Password@1");
+
+        // Tạo một đối tượng AccountDAO để thực hiện tìm kiếm
+        CustomerDAO cusDAO = new CustomerDAO();
+
+        // Gọi phương thức findAccount từ đối tượng DAO để tìm kiếm tài khoản
+        Customer found = cusDAO.findCustomer(accountToFind);
+
+        // Kiểm tra xem tài khoản đã tìm thấy hay không
+        if (found != null) {
+            System.out.println("Account found:");
+            System.out.println("ID: " + found.getCustomerID());
+            System.out.println("Name: " + found.getName());
+            System.out.println("Email: " + found.getEmail());
+            System.out.println("Role: " + found.getRoleID());
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+}
